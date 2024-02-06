8cce137d9d5e6fa438ddbdb3307b21b2cb00f363
A04pr <areed041704@gmail.com>
2024-02-05 20:34:25 -0700

    tailwind

---
+++ dev/src/components/Friends.jsx
@@ -0,0 +1,8 @@
+
+const Friends = () => {
+    return (
+    <h1>This is the friends list widget</h1>
+    );
+};
+
+export default Friends;
---
+++ dev/src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+    return (
+    <h1>This is the NavBar</h1>
+    );
+};
+
+export default NavBar;
