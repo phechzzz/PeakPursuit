5144a67374f45fb010b89ae8ba45c6532e13a245
Samuel <salvadomo@hotmail.com>
2024-02-08 14:38:23 -0700

    functionality & maintainability

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,48 +1,48 @@
-// use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
-// create a new class to instantiate for a user
 class AuthService {
-  // get user data
-  getProfile() {
-    return decode(this.getToken());
-  }
-
-  // check if user's logged in
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
-
-  // check if token is expired
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
-    }
-  }
-
+  // Retrieves the user token from localStorage
   getToken() {
-    // Retrieves the user token from localStorage
     return localStorage.getItem('id_token');
   }
 
+  // Saves user token to localStorage and redirects to home
   login(idToken) {
-    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
 
+  // Clears user token from localStorage and redirects to home
   logout() {
-    // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
     window.location.assign('/');
+  }
+
+  // Checks if the user is currently logged in
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  // Decodes and returns the profile information from the token
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // Checks if a token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      return decoded.exp < Date.now() / 1000;
+    } catch (err) {
+      // Consider logging this error or handling it further
+      return true;
+    }
+  }
+
+  // Optionally, a method to update the token stored in localStorage
+  updateToken(idToken) {
+    localStorage.setItem('id_token', idToken);
   }
 }
 
