db6271ec5e76329afa8304f5dc69df09ed15f981
phechz <peytonengborg@gmail.com>
2024-02-08 19:54:06 -0700

    pages folder and Home.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 
+import Home from './pages/Home';
+import Login from './components/LoginForm';
+import Signup from './components/SignUp';
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
+
+
+
