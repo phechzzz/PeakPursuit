b8698ba55259542bd921eed2326bfff33e975334
A04pr <areed041704@gmail.com>
2024-02-08 21:16:49 -0700

    login styling

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -30,29 +30,32 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
+      <form  className="space-y-6" onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address:</label>
+          <div className="mt-2">
+            <input
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+        <div className="mt-2">
+          <label htmlFor="pwd" className="block text-sm font-medium leading-6 text-gray-900">Password:</label>
           <input
             placeholder="******"
             name="password"
             type="password"
             id="pwd"
             onChange={handleChange}
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
         {error ? (
@@ -61,8 +64,9 @@ function Login(props) {
           </div>
         ) : null}
         <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
+          <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
         </div>
+        <Link to="/signup" className="justify-center flex font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Click Here to Sign Up!</Link>
       </form>
     </div>
   );
