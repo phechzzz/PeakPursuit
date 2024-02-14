5b2a90c3a0525b9a5a1bb13811ebd384bc40aad5
Peyton <141374563+phechzzz@users.noreply.github.com>
2024-02-14 15:45:48 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/PeakPursuit');
+const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
 
-module.exports = mongoose.connection;
+module.exports = db;
